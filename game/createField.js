1533b29f75eb0a497d883c89ca3faf21e7b07f79
slavikus39 <111385336+slavikus39@users.noreply.github.com>
2023-07-06 17:51:59 +0300

    Add files via upload

--- game/createField.js
+++ game/createField.js
@@ -1,4 +1,47 @@
+import { createMenu } from "./createMenu.js";
+
 export const createField=(lvl)=>{
+    const main = document.querySelector("main");
+    main.classList.add("none");
+
+
     console.log(lvl)
+    let lvltext;
+
+    switch(lvl){
+    case 8:
+            lvltext="ПАЦЕФИСТ";
+            break;
+        case 12:
+            lvltext="НЕЙТРАЛ";
+            break;
+        case 16:
+        lvltext="ГЕНОЦИД";
+        break;
+
+
+
+    }
+
+
+    const game = document.querySelector(".game");
+    game.style.display = 'block';
+
+    const level = game.querySelector(".lvl");
+    level.textContent = lvltext;
+
+    const menu = game.querySelector(".btn");
+    menu.addEventListener('click',()=>{
+        createMenu();
+
+    })
+
+
+
+
+
+
+
+
 }
 
--- game/createMenu.js
+++ game/createMenu.js
@@ -1,5 +1,12 @@
 import { createField } from "./createField.js";
+
 export const createMenu=()=>{
+    const main = document.querySelector("main");
+    main.classList.remove("none");
+
+    const game = document.querySelector(".game");
+    game.style.display = 'none';
+
     const menu =document.querySelector(".menu");
     menu.style.display="block";
     const btn = menu.querySelectorAll(".btn");
@@ -8,12 +15,15 @@ export const createMenu=()=>{
             console.log(el.textContent);
             switch(el.textContent) {
                 case "пацефист":
+                    menu.style.display="none";
                     createField(8)
                     break;
                 case "нейтрал":
+                    menu.style.display="none";
                     createField(12)
                     break;
                 case "геноцид":
+                    menu.style.display="none";
                     createField(16)
                     break;
             }
